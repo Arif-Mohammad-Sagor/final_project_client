7f579275478541e76d69512ac5d8e7aedd6d4c22
MdTalha707 <mdsagor0321@gmail.com>
2023-06-06 23:54:15 +0600

    navbar added

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const navItems = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      {/* <li>
+        <NavLink to="/login"> Login</NavLink>
+      </li>
+      <li>
+        <NavLink to="/signup">SignUp</NavLink>
+      </li> */}
+      <li>
+        <NavLink to="/instructors">Instructors</NavLink>
+      </li>
+      <li>
+        <NavLink to="/classes">Classes</NavLink>
+      </li>
+      <li>
+        <NavLink to="/dashboard">Dashboard</NavLink>
+      </li>
+    </>
+  );
 
-export default Navbar
+  return (
+    <div className="navbar bg-green-300">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navItems}
+          </ul>
+        </div>
+              <a className="normal-case text-2xl">
+                  Langu-Academy
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
+      </div>
+      <div className="navbar-end">
+        <Link to="/login" className="btn btn-secondary">
+          Login
+        </Link>
+        <Link to="/signup" className="btn btn-secondary">
+          SignUp
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,18 +1,38 @@
 import React from 'react'
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../layout/Main';
+import Dashboard from '../layout/Dashboard';
+import Home from '../pages/Home/Home/Home';
+import Login from '../pages/Home/Login&Signup/Login';
+import SignUp from '../pages/Home/Login&Signup/SignUp';
 
 const router = createBrowserRouter([
-  {
+    {
+      // layout for landing page
     path: "/",
         element: <Main></Main>,
         children: ([
             {
                 path: '/',
-                element:<Hom}
+                element: <Home></Home>
+
+            },
+            {
+                path: '/login',
+                element:<Login></Login>
+            },
+             {
+                path: '/signup',
+                element:<SignUp></SignUp>
+            }
     ])
-  },
+    },
+    {
+        // layouts for dashboard page
+        path: '/dashboard',
+        element:<Dashboard></Dashboard>
+    }
 ]);
 
 
-export default Routes
+export default router
