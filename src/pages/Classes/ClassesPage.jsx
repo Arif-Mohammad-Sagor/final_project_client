7322ff69b2c24b275db0ee4558bdc3ed64e5d0aa
MdTalha707 <mdsagor0321@gmail.com>
2023-06-07 18:49:48 +0600

    classes page added

--- src/pages/Classes/ClassesPage.jsx
+++ src/pages/Classes/ClassesPage.jsx
@@ -1,9 +1,79 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import useAuth from '../hooks/useAuth';
+import Swal from "sweetalert2";
+import { useNavigate } from 'react-router-dom';
 
 const ClassesPage = () => {
-  return (
-    <div>ClassesPage</div>
-  )
+  const [classes, setClasses] = useState([]);
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  // TODO: HAVE TO CREATE HOOKS FOR ADMIN AND INSTRUCTOR
+  const isAdmin = false;
+  const isInstructor = false;
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:4000/classes")
+            .then((response) => setClasses(response.data));
+    }, []);
+
+  const handleEnrollment = (id) => {
+    if (!user) {
+    Swal.fire({
+      text: "You have to loggin first",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Ok",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        navigate('/login')
+        // Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
+    }
+  }
+    return (
+      <div className="grid grid-cols-3 mx-4 my-8 gap-y-8 justify-center">
+        {classes &&
+          classes.map((classItem) => (
+            <div
+              key={classItem._id}
+              className={`card w-4/5 shadow-xl flex justify-center ${
+                classItem.AvailableSeats === 0 ? "bg-red-500" : " bg-base-100 "
+              }`}
+            >
+              <figure>
+                <img src={classItem.Image} alt="instuctors" />
+              </figure>
+              <div className="card-body">
+                <h2>CourseName: {classItem.Name}</h2>
+                <p className="text-sm">
+                  InstructorName: {classItem.InstructorName}
+                </p>
+                <p className="text-sm">
+                  AvailableSeats: {classItem.AvailableSeats}
+                </p>
+                <p className="text-sm">Price: ${classItem.Price}</p>
+                <p className="text-sm">studentQty: {classItem.studentQty}</p>
+                <div className="card-actions justify-end">
+                  <button
+                    onClick={() => handleEnrollment(`${classItem._id}`)}
+                    disabled={
+                      classItem.AvailableSeats === 0 || isAdmin || isInstructor
+                    }
+                    className="btn btn-primary"
+                  >
+                    Enroll{" "}
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    );
 }
 
 export default ClassesPage
--- src/pages/Instructors/InstructorsPage.jsx
+++ src/pages/Instructors/InstructorsPage.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const InstructorsPage = () => {
+  const [instuctors,setInstructors]=useState([])
+  useEffect(() => {
+    axios.get("http://localhost:4000/instructors")
+    .then(response =>setInstructors(response.data))
+  }, [])
+
   return (
-    <div>InstructorsPage</div>
-  )
+    <div className="grid grid-cols-3 mx-4 my-8 gap-y-8 justify-center ">
+      {instuctors &&
+        instuctors.map((instructor) => (
+
+            <div
+              key={instructor._id}
+              className="card w-4/5 flex justify-center  bg-base-100 shadow-xl"
+            >
+              <figure>
+                <img src={instructor.image} alt="instuctors" />
+              </figure>
+              <div className="card-body">
+                <h2>Name: {instructor.name}</h2>
+                <p className='text-sm'>Email: {instructor.email}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn btn-primary">Contact</button>
+                </div>
+              </div>
+            </div>
+        ))
+      }
+    </div>
+  );
 }
 
 export default InstructorsPage
